a57180e3fed2e0ec437bec60b72bfdeebac7197b
unknown <Kalobas2009@yandex.ru>
2023-09-28 14:59:35 +0300

    решены две задачи

---
+++ Seminars/Sem1/Program.cs
@@ -0,0 +1,33 @@
+﻿// System.Console.Write("Input number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine($"Quad of {num} -> {num*num} ");
+
+// System.Console.Write("Input number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine($"Quad of {num} -> {num*num} ");
+
+// 1. Напишите программу, которая принимает на вход трехзначное числои на выходе показывает **последнюю** цифру этого числа.
+
+// System.Console.Write("Input three-digit number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if(num >= 100 && num < 1000)
+// {
+//     System.Console.WriteLine($"Last digit of {num} -> {num%10}");
+// }
+// else
+// {
+//     System.Console.WriteLine("You input not three-digit number");
+// }
+
+// 2. Решите задачу
+
+// System.Console.Write("Input number: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int count = -n;
+// while (count <=n)
+// System.Console.Write (count++);
+
+// 3. Решите вторую задачу
+
+System.Console.WriteLine("Your First number: ");
