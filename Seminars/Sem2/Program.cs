b3a6cb1d7746f1a126c6204c04056bad2317bfab
unknown <Kalobas2009@yandex.ru>
2023-10-05 10:41:51 +0300

    commit HomeWorc

--- Seminars/Sem2/Program.cs
+++ Seminars/Sem2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
+﻿// 1. Первая задача
+
+// Console.WriteLine("Введите трехзначное число:");
+// int number = Convert.ToInt32(Console.ReadLine());
+// string numberS = Math.Abs(number).ToString();
+// Console.WriteLine(numberS.Length == 3 ? 
+// $"{number} -> {numberS[1]}" : "Вы ввели не трехзначное число");
+
+
+// 2. Вторая задача
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+string numberS = Math.Abs(number).ToString();
+
+Console.WriteLine(numberS.Length > 2 ? 
+$"{number} -> {numberS[2]}" : $"{numberS} -> третьей цифры нет");
+
